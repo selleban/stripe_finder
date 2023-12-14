80a9880af5ac61983c77a28643a537224f78d9b1
Selle Bandstra <69871668+selleban@users.noreply.github.com>
2023-12-14 19:34:09 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,21 +2,17 @@
 import os
 from setuptools import setup
 
-# Utility function to read the README file.
-# Used for the long_description.  It's nice, because now 1) we have a top level
-# README file and 2) it's easier to type in the README file than to put a raw
-# string in below ...
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "mean",
+    name = "src",
     version = "0.0.4",
-    author = "Ferdinand van Wyk",
-    description = ("An example of how to set up pytest and Sphinx."),
+    author = "Selle Bandstra",
+    description = ("Simple python package to find stripy patterns in neurons"),
     license = "GNU",
     keywords = "example pytest tutorial sphinx",
-    packages=['mean', 'tests', 'doc'],
+    packages=['src', 'test'],
     long_description=read('README.md'),
     classifiers=[
         "Development Status :: 3 - Alpha",
